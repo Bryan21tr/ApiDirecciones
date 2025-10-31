8e857bfca10185ad17462776f279231540008673
Bryan21tr <bryan21bye@gmail.com>
2025-10-31 12:41:21 -0600

    Fix: se creo la rama EBHA y se agragaron los campos en BD

--- Model/Entities/DireccionesEntidad.cs
+++ Model/Entities/DireccionesEntidad.cs
@@ -11,17 +11,14 @@ namespace APIDirecciones.Model.Entities
 
         public string? Calle { get; set; }
         public string? Colonia { get; set; }
-        
         public string? Municipio { get; set; }
-
-        public int? Numero { get; set; }
-        
+        public int? Numero_Ext { get; set; }
+        public int? Numero_Int { get; set; }
         public string? CP { get; set; }
-
+        public string? Entre_Calles { get; set; }
+        public string? Descripcion { get; set; }
         public Boolean? Activo { get; set; }
-
         public DateTime? Fecha_modificacion { get; set; }
-        
         public DateTime? Fecha_creacion { get; set; }
     }
 }
